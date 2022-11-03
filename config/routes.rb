e4be489988d352689b4aaa492bd467434a8540b2
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-11-03 18:52:47 +1100

    added login feature through session controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   get 'welcome', to: 'sessions#welcome'
-
+  get 'authorized', to: 'sessions#page_requires_login'
   # get 'categories/index'
   # get 'categories/new'
   # get 'categories/create'
