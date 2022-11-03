ec0a382ba79a1c68a552c916af9fe1261dc59f19
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-11-03 17:30:14 +1100

    added rspec gem and created spec for user model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+# require 'rails_helper'
+
+# RSpec.describe User, type: :model do
+#   # pending "add some examples to (or delete) #{__FILE__}"
+#   let (:attributes) do
+#     {
+#       email: 'test@example.com',
+#       password: 'test',
+#       first_name: 'Test',
+#       last_name: 'TestLast'
+#     }
+#   end
+#   let (:missing_first_name) { attributes.except(:first_name) }
+#   let (:invalid_email) { attributes.merge(email: "wrong.email.example.com") }
+
+#   it "is valid" do
+#     expect(User.new(attributes)).to be_valid
+#   end
+
+#   it "is not valid without first name" do
+#     expect(User.new(missing_first_name)).not_to be_valid
+#   end
+
+#   it "is not valid with wrong email" do
+#     expect(User.new(invalid_email)).not_to be_valid
+#   end
+# end
